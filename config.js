c816d67c45099b0a25c8c2d56ace7b9dc070a10e
Jan Vincent Junsay <jvjunsay@Jans-Mac-mini.local>
2020-05-03 19:44:39 +0800

    removed aws config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    aws_key_id:'AKIAIAGMDQPDSWR7OHQA',
-    aws_secret:'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',
+    aws_key_id:'*',
+    aws_secret:'*',
     bucket: 'meldcxtest1',
     region: 'ap-southeast-1'
 };
